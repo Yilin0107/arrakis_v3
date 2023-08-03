cd288ff76ec9477a47bc7822627da4ca3e6c5a49
Murtaza Wani <murtazawani7@gmail.com>
2023-08-03 16:37:50 +0100

    bonds details page data fixed

--- react-app/src/components/BondsDetail.jsx
+++ react-app/src/components/BondsDetail.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
 
 const BondsDetail = (props) => {
+  // Assuming props.info.bondMaturityDate contains the date string "2021-08-04T23:00:00.000+00:00"
+  const bondMaturityDate = new Date(props.info.bondMaturityDate);
+  const formattedDate = bondMaturityDate.toISOString().split('T')[0];
 
   return (
     <div>
       <Card>
         <Card.Body>
-      <Card.Title><strong>ISIN:</strong> {props.info.isin}</Card.Title>
-      <Card.Text>
-      <p><strong>Bond Currency:</strong> {props.info.currency}</p> 
-      <p><strong>CUSIP:</strong> {props.info.cusip} </p>
-      <p><strong>Issuer Name:</strong> {props.info.issuerName} </p>
-      <p><strong>Bond Maturity Date:</strong> {props.info.maturityDate} </p>
-      <p><strong>Status:</strong> {props.info.status} </p>
-      <p><strong>Type:</strong> {props.info.type}</p>
-  
-      </Card.Text>
-     </Card.Body>
+          <Card.Title><strong>ISIN:</strong> {props.info.isin}</Card.Title>
+          <Card.Text>
+            <strong>Bond Currency:</strong> {props.info.bondCurrency} <br />
+            <strong>CUSIP:</strong> {props.info.cusip}<br />
+            <strong>Issuer Name:</strong> {props.info.issuerName}<br />
+            <strong>Bond Maturity Date:</strong> {formattedDate}<br />
+            <strong>Status:</strong> {props.info.status}<br />
+            <strong>Type:</strong> {props.info.type}<br />
+            <strong>Face Value:</strong> {props.info.faceValue}<br />
+          </Card.Text>
+        </Card.Body>
       </Card>
     </div>
   );
