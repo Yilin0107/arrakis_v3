e3bde3473bcb7b1ba49ada22b5da204e002b2c67
georgiana-stoica <sgeo2001@gmail.com>
2023-08-03 16:33:05 +0100

    login and navigation page

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,14 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import './App.css';
 import AllBonds from "./components/AllBonds";
+import SignIn from './components/SignIn';
+import { Dashboard } from '@mui/icons-material';
+import FinancialDashboard from './components/FinancialDashboard';
 
 const App = () => {
   return (
     <div>
       
-  
-    <AllBonds />
-
+    <FinancialDashboard />
 
     </div>
   
---
+++ react-app/src/components/TradesDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TradesDetails = () => {
+  return (
+    <div>TradesDetails</div>
+  )
+}
+
+export default TradesDetails
