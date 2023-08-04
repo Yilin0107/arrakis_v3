e8b8a153fd7d5d4e5d5f78795afe3eb839f0c961
Murtaza Wani <murtazawani7@gmail.com>
2023-08-04 10:26:12 +0100

    frontend some filters added

--- react-app/src/components/AllBonds.jsx
+++ react-app/src/components/AllBonds.jsx
@@ -4,234 +4,86 @@ import Row from 'react-bootstrap/Row';
 import { getAllBonds } from '../services/bond-service';
 
 const AllBonds = () => {
-    // const bonds = [
-    //     {
-    //       isin: 'XS1988387210',
-    //       currency: 'USD',
-    //       cusip: null,
-    //       faceValue: '1000',
-    //       issuerName: 'BNPParibasIssu 4,37% Microsoft Corp (USD)',
-    //       maturityDate: '2021-08-05',
-    //       status: 'active',
-    //       type: 'CORP',
-    //     },
-    //     {
-    //       isin: 'USN0280EAR64',
-    //       currency: 'USD',
-    //       cusip: '123456780',
-    //       faceValue: '900',
-    //       issuerName: 'Airbus 3.15% USD',
-    //       maturityDate: '2021-07-30',
-    //       status: 'active',
-    //       type: 'CORP',
-    //     },
-    //     {
-    //       isin: 'A12356111',
-    //       currency: 'USD',
-    //       cusip: '123456bh0',
-    //       faceValue: '900',
-    //       issuerName: 'UBS Facebook (USD)',
-    //       maturityDate: '2021-09-30',
-    //       status: 'active',
-    //       type: 'CORP',
-    //     },
-    //     {
-    //       isin: 'USU02320AG12',
-    //       currency: 'USD',
-    //       cusip: null,
-    //       faceValue: '900',
-    //       issuerName: 'Amazon',
-    //       maturityDate: '2021-08-03',
-    //       status: 'active',
-    //       type: 'CORP',
-    //     },
-    //     {
-    //       isin: 'GB00B6460505',
-    //       currency: 'GBP',
-    //       cusip: 'BDCHBW8',
-    //       faceValue: '900',
-    //       issuerName: 'HM Treasury United Kingdom',
-    //       maturityDate: '2021-08-09',
-    //       status: 'active',
-    //       type: 'GOVN',
-    //     },
-
-    //     {
-    //         isin: 'GB00B6460506',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-          
-    //       {
-    //         isin: 'GB00B6460507',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460508',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460509',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460510',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460511',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460512',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460513',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460514',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-    //       {
-    //         isin: 'GB00B6460515',
-    //         currency: 'GBP',
-    //         cusip: 'BDCHBW8',
-    //         faceValue: '900',
-    //         issuerName: 'HM Treasury United Kingdom',
-    //         maturityDate: '2021-08-09',
-    //         status: 'active',
-    //         type: 'GOVN',
-    //       },
-
-    //       {
-    //         isin: 'US87973RAA86',
-    //         currency: 'USD',
-    //         cusip: '87973RAA8',
-    //         faceValue: '690',
-    //         issuerName: 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S',
-    //         maturityDate: '2021-08-06',
-    //         status: 'active',
-    //         type: 'SOVN',
-    //       },
-    //       {
-    //         isin: 'IE00B29LNP31',
-    //         currency: 'USD',
-    //         cusip: '87973RAA8',
-    //         faceValue: '340',
-    //         issuerName: 'First Norway Alpha Kl.IV',
-    //         maturityDate: '2030-12-22',
-    //         status: 'active',
-    //         type: 'SOVN',
-    //       },
-    //       {
-    //         isin: 'CE00B29LNP67',
-    //         currency: 'USD',
-    //         cusip: '98973RAA7',
-    //         faceValue: '360',
-    //         issuerName: 'ABC Airways',
-    //         maturityDate: '2030-11-12',
-    //         status: 'active',
-    //         type: 'CORP',
-    //       },
-    //       {
-    //         isin: 'M12356111653',
-    //         currency: 'GBP',
-    //         cusip: '333456bh0',
-    //         faceValue: '700',
-    //         issuerName: 'UBS MEWS',
-    //         maturityDate: '2021-08-30',
-    //         status: 'active',
-    //         type: 'CORP',
-    //       },
-          
-       
-    //   ];
   const [bonds, setBonds] = useState([]);
+  const [filterType, setFilterType] = useState(''); // Filter by bond type (e.g., 'CORP', 'GOVN', 'SOVN')
+  const [filterCurrency, setFilterCurrency] = useState('');
+  const [filterIssuerName, setFilterIssuerName] = useState('')
+  const [filterEmployeeId, setEmployeeID] =  useState('') // Filter by currency (e.g., 'USD', 'GBP')
+  // Add more state variables for other filter criteria if needed
 
-  useEffect( () =>{
-    getBondsFromAPI();},
-    []
-  );
+  useEffect(() => {
+    getBondsFromAPI();
+  }, []);
 
   const getBondsFromAPI = () => {
     getAllBonds()
-  .then(res => {
-      setBonds(res.data);
-  })
-  .catch(err => {
-      setBonds([]);
-      console.log(err);
-  })
-  }
+      .then(res => {
+        setBonds(res.data);
+      })
+      .catch(err => {
+        setBonds([]);
+        console.log(err);
+      });
+  };
+
+  // Filter the bonds based on the selected criteria
+  const filteredBonds = bonds.filter(bond => {
+    // Filter by bond type
+    if (filterType && bond.type !== filterType) {
+      return false;
+    }
+    // Filter by currency
+    if (filterCurrency && bond.bondCurrency !== filterCurrency) {
+      return false;
+    }
+    if (filterIssuerName && bond.issuerName !== filterIssuerName) {
+          return false;
+        }
+    if (filterEmployeeId && bond.employeeId !== filterEmployeeID) {
+               return false;
+
+    // Add more filtering conditions here for other criteria if needed
+    return true;
+  });
 
   return (
-    <Row>
-    {bonds.map(bond=>(
-        <div className='container' key={bond.isin}>
-        <BondsDetail info={bond}  />
+    <div>
+      <div>
+        <label>Filter by Type:</label>
+        <select
+          value={filterType}
+          onChange={e => setFilterType(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="CORP">CORP</option>
+          <option value="GOVN">GOVN</option>
+          <option value="SOVN">SOVN</option>
+          {/* Add more options based on your bond types */}
+        </select>
+      </div>
+      <div>
+        <label>Filter by Currency:</label>
+        <select
+          value={filterCurrency}
+          onChange={e => setFilterCurrency(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+          {/* Add more options based on your bond currencies */}
+        </select>
+      </div>
 
-        </div>
-    ))}
-    </Row>
-  
-    )
-}
 
-export default AllBonds
+      <Row>
+        {filteredBonds.map(bond => (
+          <div className='container' key={bond.isin}>
+            <BondsDetail info={bond} />
+          </div>
+        ))}
+      </Row>
+    </div>
+  );
+};
 
-// <BondsDetail info={bonds}/>
+export default AllBonds;
