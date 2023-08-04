26091fc0d361ca4c95a6f2d78753c94f53a51700
Murtaza Wani <murtazawani7@gmail.com>
2023-08-04 16:39:42 +0100

    Bond Detail Page

--- react-app/src/components/AllBonds.jsx
+++ react-app/src/components/AllBonds.jsx
@@ -1,6 +1,5 @@
-import React , {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import BondsDetail from './BondsDetail';
-import Test from './Test';
 import Row from 'react-bootstrap/Row';
 import { getAllBonds } from '../services/bond-service';
 
@@ -8,8 +7,9 @@ const AllBonds = () => {
   const [bonds, setBonds] = useState([]);
   const [filterType, setFilterType] = useState(''); // Filter by bond type (e.g., 'CORP', 'GOVN', 'SOVN')
   const [filterCurrency, setFilterCurrency] = useState('');
-  const [filterIssuerName, setFilterIssuerName] = useState('')
-  const [filterEmployeeId, setFilterEmployeeId] =  useState('') // Filter by currency (e.g., 'USD', 'GBP')
+  const [filterEmployeeId, setFilterEmployeeId] = useState(''); // Filter by currency (e.g., 'USD', 'GBP')
+  const [showFilters, setShowFilters] = useState(false);
+
   // Add more state variables for other filter criteria if needed
 
   useEffect(() => {
@@ -38,70 +38,112 @@ const AllBonds = () => {
       return false;
     }
 
-    if (filterEmployeeId && bond.employeeId.toString !== filterEmployeeId) {
-               return false;
+    if (filterEmployeeId && bond.employeeId.toString() !== filterEmployeeId) {
+      return false;
     }
     // Add more filtering conditions here for other criteria if needed
     return true;
   });
 
-  return (
-    <div>
-      <div>
-        <label>Filter by Type:</label>
-        <select
-          value={filterType}
-          onChange={e => setFilterType(e.target.value)}
-        >
-          <option value="">All</option>
-          <option value="CORP">CORP</option>
-          <option value="GOVN">GOVN</option>
-          <option value="SOVN">SOVN</option>
-          {/* Add more options based on your bond types */}
-        </select>
-      </div>
-      <div>
-        <label>Filter by Currency:</label>
-        <select
-          value={filterCurrency}
-          onChange={e => setFilterCurrency(e.target.value)}
-        >
-          <option value="">All</option>
-          <option value="USD">USD</option>
-          <option value="GBP">GBP</option>
-          {/* Add more options based on your bond currencies */}
-        </select>
-      </div>
+   const toggleFilters = () => {
+     setShowFilters(!showFilters);
+   };
 
-       <div>
-              <label>Filter by Employee Id:</label>
-              <select
-                value={filterEmployeeId}
-                onChange={e => setFilterType(e.target.value)}
-              >
-                <option value="">All</option>
-                <option value="1000">1000</option>
-                <option value="1001">1001</option>
-                <option value="1002">1002</option>
-                <option value="1003">1003</option>
-                <option value="1004">1004</option>
-              </select>
-            </div>
+  const filterContainerStyle = {
+      display: 'flex',
+      alignItems: 'center',
+      marginBottom: '0.5rem',
+      overflow: 'auto'
+    };
 
-            <div>
-            <Test />
-            </div>
+    const filterLabelStyle = {
+      marginRight: '0.5rem',
+      fontWeight: 'bold',
+    };
+
+    const blueBandStyle = {
+      backgroundColor: '#0018a8',
+      color: 'white',
+      padding: '10px',
+      textAlign: 'center',
+    };
+
+    //
 
 
-      <Row>
-        {filteredBonds.map(bond => (
-          <div className='container' key={bond.isin}>
-            <BondsDetail info={bond} />
+
+
+
+    return (
+     <div>
+          {/* Top band */}
+          <div style={blueBandStyle}>
+            <h1>Bonds Detail Page</h1>
           </div>
-        ))}
-      </Row>
-    </div>
-  );
-};
+          <div>
+          <br />
+          </div>
+               <div
+                 className="filter-card"
+                 style={{ backgroundColor: 'grey', padding: '10px', cursor: 'pointer', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+                 onClick={toggleFilters}
+               >
+                 <h5>Filter Options</h5>
+                 <div
+                   className="arrow-icon"
+                   style={{ width: 0, height: 0, borderLeft: '5px solid transparent', borderRight: '5px solid transparent', borderTop: '8px solid white' }}
+                 ></div>
+               </div>
 
-export default AllBonds;
+               {showFilters && (
+                 <div
+                   className="filter-options"
+                   style={{ display: 'flex', backgroundColor: '#f9f9f9', padding: '10px', border: '1px solid #ccc', borderRadius: '4px' }}
+                 >
+                   <div className="filter-container">
+                     <label className="filter-label">Filter by Type:</label>
+                     <select value={filterType} onChange={(e) => setFilterType(e.target.value)}>
+                       <option value="">All</option>
+                       <option value="CORP">CORP</option>
+                       <option value="GOVN">GOVN</option>
+                       <option value="SOVN">SOVN</option>
+                     </select>
+                   </div>
+
+                   <div className="filter-container">
+                     <label className="filter-label">Filter by Currency:</label>
+                     <select value={filterCurrency} onChange={(e) => setFilterCurrency(e.target.value)}>
+                       <option value="">All</option>
+                       <option value="USD">USD</option>
+                       <option value="GBP">GBP</option>
+                     </select>
+                   </div>
+
+                   <div className="filter-container">
+                     <label className="filter-label">Filter by Employee Id:</label>
+                     <select value={filterEmployeeId} onChange={(e) => setFilterEmployeeId(e.target.value)}>
+                       <option value="">All</option>
+                       <option value="1000">1000</option>
+                       <option value="1001">1001</option>
+                       <option value="1002">1002</option>
+                       <option value="1003">1003</option>
+                       <option value="1004">1004</option>
+                     </select>
+                   </div>
+                 </div>
+               )}
+
+
+
+        <Row>
+          {filteredBonds.map(bond => (
+            <div className='container' key={bond.isin}>
+              <BondsDetail info={bond} />
+            </div>
+          ))}
+        </Row>
+      </div>
+    );
+  };
+
+  export default AllBonds;
--- react-app/src/components/BondsDetail.jsx
+++ react-app/src/components/BondsDetail.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import '../BondsDetail.css'; // Import your custom CSS for styling
 
 const BondsDetail = (props) => {
   // Assuming props.info.bondMaturityDate contains the date string "2021-08-04T23:00:00.000+00:00"
   const bondMaturityDate = new Date(props.info.bondMaturityDate);
   const formattedDate = bondMaturityDate.toISOString().split('T')[0];
 
+  const [expanded, setExpanded] = useState(false);
+
+  const handleCardClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div>
-      <Card>
+      <Card className={`bond-card ${expanded ? 'expanded' : ''}`} onClick={handleCardClick}>
         <Card.Body>
           <Card.Title><strong>ISIN:</strong> {props.info.isin}</Card.Title>
-          <Card.Text>
-            <strong>Bond Currency:</strong> {props.info.bondCurrency} <br />
-            <strong>CUSIP:</strong> {props.info.cusip}<br />
-            <strong>Issuer Name:</strong> {props.info.issuerName}<br />
-            <strong>Bond Maturity Date:</strong> {formattedDate}<br />
-            <strong>Status:</strong> {props.info.status}<br />
-            <strong>Type:</strong> {props.info.type}<br />
-            <strong>Face Value:</strong> {props.info.faceValue}<br />
-            <strong>Employee Id:</strong> {props.info.employeeId}<br />
-          </Card.Text>
+          {!expanded && (
+            <Card.Text>
+              <strong>Issuer Name:</strong> {props.info.issuerName}<br />
+            </Card.Text>
+          )}
+          {expanded && (
+            <Card.Text>
+              <strong>Issuer Name:</strong> {props.info.issuerName}<br />
+              <strong>Bond Currency:</strong> {props.info.bondCurrency} <br />
+              <strong>CUSIP:</strong> {props.info.cusip}<br />
+              <strong>Bond Maturity Date:</strong> {formattedDate}<br />
+              <strong>Status:</strong> {props.info.status}<br />
+              <strong>Type:</strong> {props.info.type}<br />
+              <strong>Face Value:</strong> {props.info.faceValue}<br />
+              <strong>Employee Id:</strong> {props.info.employeeId}<br />
+            </Card.Text>
+          )}
         </Card.Body>
+        <div className={`expand-arrow ${expanded ? 'expanded' : ''}`}>
+          <span>▼</span>
+        </div>
       </Card>
     </div>
   );
