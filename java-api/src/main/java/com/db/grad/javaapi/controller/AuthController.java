cc063664a8eb7ff78c6d87189b21dfa291ae84d8
Yilin0107 <liuyilin0107@gmail.com>
2023-08-07 13:51:53 +0200

    added crossorigin

--- java-api/src/main/java/com/db/grad/javaapi/controller/AuthController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/AuthController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class AuthController {
 
     private final EmployeeServiceImpl employeeServiceImpl;
