9ac4225201a6a7f7c47f83403eebf9308d02de0d
Yilin0107 <liuyilin0107@gmail.com>
2023-08-07 12:26:30 +0200

    added login page

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,19 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './services/SignIn';
 import AllBonds from "./components/AllBonds";
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//
+//
+//     <AllBonds />
+//
+//
+//     </div>
+//
+//
+//   );
+// }
+//
+// export default App;
+
+function App() {
   return (
-    <div>
-      
-  
-    <AllBonds />
-
-
-    </div>
-  
-
+      <Router>
+          <Routes>
+              <Route path="/" element={<SignIn />} />
+              <Route path="/bonds" element={<AllBonds />} />
+          </Routes>
+      </Router>
   );
 }
 
